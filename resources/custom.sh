c0adc92aa9c97dbb30489f2f625f0a1625e3fc86
MuldeR <mulder2@gmx.de>
2026-01-14 14:14:03 +0100

    Use 'pkgin' utility to upgrade and install packages.

--- resources/custom.sh
+++ resources/custom.sh
@@ -6,15 +6,15 @@ export RUSTUP_HOME=/opt/rust/rustup
 export CARGO_HOME=/opt/rust/cargo
 
 # Add your additional provisioning here for custom VM images.
-/usr/sbin/pkg_add -u
-/usr/sbin/pkg_add git curl clang
+pkgin update
+pkgin upgrade
+pkgin -y install clang
 
 mkdir -p /opt/sysroot/i386
-
 curl -sSf https://cdn.netbsd.org/pub/NetBSD/NetBSD-10.1/i386/binary/sets/base.tgz | tar -C /opt/sysroot/i386 -xzf - lib usr/lib
 curl -sSf https://cdn.netbsd.org/pub/NetBSD/NetBSD-10.1/i386/binary/sets/comp.tgz | tar -C /opt/sysroot/i386 -xzf - usr/lib
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.92.0
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.92.0
 /opt/rust/cargo/bin/rustup component add rust-src
 
 echo '#!/bin/sh' > /opt/rust/cargo/env
