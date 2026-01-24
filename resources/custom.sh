bbbeb90779e5ffde61f685b758cdd59a7dfe1d70
MuldeR <mulder2@gmx.de>
2026-01-24 03:35:22 +0100

    Updated Rust version to 1.93.0.

--- resources/custom.sh
+++ resources/custom.sh
@@ -14,7 +14,7 @@ mkdir -p /opt/sysroot/i386
 curl -sSf https://cdn.netbsd.org/pub/NetBSD/NetBSD-10.1/i386/binary/sets/base.tgz | tar -C /opt/sysroot/i386 -xzf - lib usr/lib
 curl -sSf https://cdn.netbsd.org/pub/NetBSD/NetBSD-10.1/i386/binary/sets/comp.tgz | tar -C /opt/sysroot/i386 -xzf - usr/lib
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.92.0
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.93.0
 /opt/rust/cargo/bin/rustup component add rust-src
 
 echo '#!/bin/sh' > /opt/rust/cargo/env
